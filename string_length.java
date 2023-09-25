3b4fa561ee5894fe89f913fe9c54b31f434dff87
Prakriti Kanchan <122687870+PrakritiKanchan@users.noreply.github.com>
2023-09-26 00:32:00 +0530

    Create string_length.java

---
+++ string_length.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class string_length {
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+       String name ="Prakriti";
+       int length1=name.length();
+       System.out.println("The length of String  " + length1); 
+    }
+}
