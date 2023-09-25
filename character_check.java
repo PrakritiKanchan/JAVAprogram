ad4f221e6cfb127fc1e3927f698f562d74c73bc9
Prakriti Kanchan <122687870+PrakritiKanchan@users.noreply.github.com>
2023-09-25 23:44:42 +0530

    Create character_check.java

---
+++ character_check.java
@@ -0,0 +1,11 @@
+public class character_check {
+    public static void main(String[] args) {
+        char c = 'a';
+        if (Character.isAlphabetic(c)){
+            System.out.println("Character is Alphabate");
+        }
+        else{
+            System.out.print("Character is not a Alphabate");
+        }
+    }
+}
