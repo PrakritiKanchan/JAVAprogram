63ca715d54e7f520f521290069b27e2967b63786
Prakriti Kanchan <122687870+PrakritiKanchan@users.noreply.github.com>
2023-09-26 00:00:59 +0530

    Create length_string.java

---
+++ length_string.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class lengthStirng
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a string : ");
+        String a = sc.next();
+        System.out.println("Enter a second string : ");
+        String b = sc.next();
+        System.out.println(a.length());
+        System.out.println(b.length());
+
+
+    }
+}
